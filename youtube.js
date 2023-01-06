1493cd274138cc0b490d1f99c2a3290afe080e5b
Filip Gregor <filip.gregor98@gmail.com>
2023-01-06 18:16:50 +0100

    Remove shorts and fix hiding videos on user pages

--- youtube.js
+++ youtube.js
@@ -1,8 +1,31 @@
-const removeHTML = () => {
-    const videoGrids = document.getElementsByTagName('ytd-rich-grid-renderer');
-    for (grid of videoGrids) {
-        grid.style.display = 'none';
+
+const remove_grid = () => {
+    // Remove grid of videos
+    var style = document.createElement('style');
+    style.innerHTML = `
+    ytd-rich-grid-renderer {
+        display: none
+    }
+    `;
+
+    if (!document.location.pathname.match(/\/@.*/)) {
+        document.head.appendChild(style);
+    }
+}
+
+remove_grid();
+
+
+const remove_shorts = () => {
+    // Remove shorts menu
+    let entries = document.getElementsByTagName('ytd-mini-guide-entry-renderer');
+
+    for (obj of entries) {
+        if (obj.getAttribute('aria-label') == 'Shorts') {
+            obj.style.display = 'none';
+        }
     }
 };
 
-setInterval(removeHTML, 100);
+// This is stupid and inefficient, use MutationObserver
+setInterval(remove_shorts, 50);
